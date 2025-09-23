856f89a588727c186496e38caf237bd40abba2f9
rifki <rifki.vendor.pasti@pegadaian.co.id>
2025-09-24 02:52:22 +0700

    complete payment flow

---
+++ src/main/java/com/roomledger/app/exthandler/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.roomledger.app.exthandler;
+
+public class DataAccessException extends RuntimeException {
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
+
